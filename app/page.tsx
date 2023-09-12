9ffc97c12ac2a81521bc1fe3690ab5bed2276be8
bhsunilvarma13 <mailme4loops@gmail.com>
2023-09-12 18:13:57 +0530

    feat: landing page added

--- app/page.tsx
+++ app/page.tsx
@@ -11,10 +11,10 @@ export default async function Home() {
       </div>
       <div className="max-w-5xl mx-auto h-[80vh] grid place-items-center px-4">
         <div className="flex flex-col items-center gap-8">
-          <h1 className="text-7xl font-black text-center">
+          <h1 className="text-3xl font-black text-center sm:text-7xl">
             Revamp your homework experience
           </h1>
-          <p className="text-base w-3/4 text-center font-light leading-relaxed">
+          <p className="sm:text-base text-sm sm:w-3/4 w-full text-center font-light leading-relaxed">
             Elevate the way you create and distribute homework tasks with
             HomeWorkMail. Our cutting-edge SaaS platform streamlines the entire
             process, making it seamless for educators to craft, allocate, and
